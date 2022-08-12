4d83564d8d4a988594181943fe76293fb643372f
idhun90 <idhun90@naver.com>
2022-08-13 03:05:49 +0900

    스토리보드 UI 구성

--- SeSAC_iOS_OpenWeather/OpenWeatherViewController.swift
+++ SeSAC_iOS_OpenWeather/OpenWeatherViewController.swift
@@ -1,29 +1,9 @@
-//
-//  OpenWeatherViewController.swift
-//  SeSAC_iOS_OpenWeather
-//
-//  Created by 이도헌 on 2022/08/13.
-//
-
 import UIKit
 
 class OpenWeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
